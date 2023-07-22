5abe4f54a3ed9b2077792dcc7d62502cc9cd60c8
Mayank Mishra <89137604+mmayank591@users.noreply.github.com>
2023-07-22 09:03:03 +0530

    Create detect_a_loop_in_a_linked_list.java

---
+++ detect_a_loop_in_a_linked_list.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class Node{
+        int data;
+        Node next;
+        Node(int x){
+            data=x;
+            next=null;
+        }
+    }
+  
+class  detect_loop_in_linked_list { 
+    
+    public static void main(String args[]) 
+    { 
+        Node head=new Node(15);
+    	head.next=new Node(10);
+    	head.next.next=new Node(12);
+    	head.next.next.next=new Node(20);
+    	head.next.next.next.next=head.next;
+    	if (isLoop(head)) 
+            System.out.print("Loop found"); 
+        else
+            System.out.print("No Loop"); 
+    } 
+    
+    static boolean isLoop(Node head) 
+    {   HashSet<Node> s=new HashSet<Node>(); 
+        for(Node curr=head;curr!=null;curr=curr.next) {  
+            if (s.contains(curr)) 
+                return true; 
+            s.add(curr); 
+        } 
+        return false;  
+    }
+    
+    public static void printlist(Node head){
+        Node curr=head;
+        while(curr!=null){
+        System.out.print(curr.data+" ");
+        curr=curr.next;
+    }System.out.println();
+    }
+} 
