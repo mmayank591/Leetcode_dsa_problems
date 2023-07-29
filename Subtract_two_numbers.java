ee053353de9a8f05fc8fbea10333d8cacba9fdee
Mayank Mishra <89137604+mmayank591@users.noreply.github.com>
2023-07-29 23:24:50 +0530

    Subtract_two_numbers.java

---
+++ Subtract_two_numbers.java
@@ -0,0 +1,20 @@
+public class SubtractTwoNumbers {
+ 
+    public static void main(String[] args) {
+         
+        // declare and initialize two integer numbers
+        int x=50;
+        int y=10;
+ 
+        // declare another variable to hold the substracted value
+        int subtract;
+ 
+        // calculate the substraction 
+        subtract = x - y;
+ 
+        // print the calculated subtract value
+        System.out.println("The substraction of two Numbers: " + subtract);
+ 
+    }
+ 
+}
