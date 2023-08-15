ab7f1fffecd28a3451e034f171693f7bf0ef4e6e
Mayank Mishra <89137604+mmayank591@users.noreply.github.com>
2023-08-15 20:45:54 +0530

    Create Delete Greatest Value in EachRow.java

---
+++ Delete Greatest Value in EachRow.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int deleteGreatestValue(int[][] grid) {
+        int ans=0;
+for(int i=0;i<grid.length;i++){
+Arrays.sort(grid[i]);
+}
+
+for(int col=0;col<grid[0].length;col++){
+        int max=0;
+
+for(int row=0;row<grid.length;row++){
+        if(grid[row][col]>max)
+max=grid[row][col];
+
+}
+ans+=max;
+}   
+return ans;         
+}
+
+
+
+}
+
+
