0f6ff6cb3f73b24d9eb82ebb4e3923e5815e1424
Mayank Mishra <89137604+mmayank591@users.noreply.github.com>
2023-08-17 16:10:48 +0530

    Create 326. Power of Three.java

---
+++ 326. Power of Three.java
@@ -0,0 +1,17 @@
+question link-->>https://leetcode.com/problems/power-of-three/description/
+
+////////////////////////////////////////SOLUTION///////////////////////////////////
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n==1){
+            return true;
+        }
+        if(n<=0){
+            return false;
+        }
+        while(n%3==0){
+            n=n/3;
+        }
+        return (n==1);
+    }
+}
