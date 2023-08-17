fcf5cb3afa36f060447f04c66acf50dc09437348
Mayank Mishra <89137604+mmayank591@users.noreply.github.com>
2023-08-17 16:16:34 +0530

    Create 342. Power of Four.java

---
+++ 342. Power of Four.java
@@ -0,0 +1,16 @@
+question link--->>https://leetcode.com/problems/power-of-four/description/
+
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n==1){
+            return true;
+        }
+        if(n<=0){
+            return false;
+        }
+        while(n%4==0){
+            n=n/4;
+        }
+        return (n==1);
+    }
+}
