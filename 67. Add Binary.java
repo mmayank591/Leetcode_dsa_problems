0a98ecebae61f47be4e55d021facbca78b896561
Mayank Mishra <89137604+mmayank591@users.noreply.github.com>
2023-09-01 22:43:22 +0530

    Create 67. Add Binary.java

---
+++ 67. Add Binary.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String addBinary(String a, String b) {
+
+      int alen=a.length();
+      int blen=b.length();
+int carry=0,i=0;
+  String result="";  
+    while(i<alen||i<blen||carry!=0){
+        int x=0;
+        if(i<alen && a.charAt(alen-1-i)=='1')
+        x=1;
+        int y=0;
+        if(i<blen && b.charAt(blen-1-i)=='1')
+        y=1;
+
+        result=(x+y+carry)%2+result;
+        carry=(x+y+carry)/2;
+        i++;
+
+
+    }
+     
+return result;
+
+
+
+    }
+}
