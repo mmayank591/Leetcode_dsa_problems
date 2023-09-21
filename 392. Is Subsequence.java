bc1ecab1b5d479356b51757ab57ebb11bba1f987
Mayank Mishra <89137604+mmayank591@users.noreply.github.com>
2023-09-21 11:51:35 +0530

    Create 392. Is Subsequence.java

---
+++ 392. Is Subsequence.java
@@ -0,0 +1,18 @@
+https://leetcode.com/problems/is-subsequence/description/
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        
+ int si=0;
+         if(s.length()<1)
+             return true;
+         for(int i=0;i<t.length();i++){
+             if(s.charAt(si)==t.charAt(i))
+                 si++;
+
+             if(si==s.length())
+                 return true;
+        }
+
+    return false;
+}}
