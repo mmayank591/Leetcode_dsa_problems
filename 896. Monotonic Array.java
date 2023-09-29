376bd1910913472dae75a840edd62b1634a3c0b9
Mayank Mishra <89137604+mmayank591@users.noreply.github.com>
2023-09-29 10:34:45 +0530

    Create 896. Monotonic Array.java

---
+++ 896. Monotonic Array.java
@@ -0,0 +1,21 @@
+questionlink-->>https://leetcode.com/problems/monotonic-array/description/?envType=daily-question&envId=2023-09-29
+
+public class Solution {
+    public boolean isMonotonic(int[] nums) {
+        if (nums.length < 2) return true;
+
+        int direction = 0;  // 0 means unknown, 1 means increasing, -1 means decreasing
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i-1]) {  // increasing
+                if (direction == 0) direction = 1;
+                else if (direction == -1) return false;
+            } else if (nums[i] < nums[i-1]) {  // decreasing
+                if (direction == 0) direction = -1;
+                else if (direction == 1) return false;
+            }
+        }
+
+        return true;
+    }
+}
