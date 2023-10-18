488c23236848c40d59c20bf757f35e3a75d9cdfd
Mayank Mishra <89137604+mmayank591@users.noreply.github.com>
2023-10-18 08:48:19 +0530

    Create 674. Longest Continuous Increasing Subsequence.java

---
+++ 674. Longest Continuous Increasing Subsequence.java
@@ -0,0 +1,20 @@
+https://leetcode.com/problems/longest-continuous-increasing-subsequence/description/
+
+class Solution {
+    public int findLengthOfLCIS(int[] nums) {
+        int max = 1;
+        int count = 1;
+        
+        for(int i = 1 ; i < nums.length; i++) {
+            if (nums[i - 1] < nums[i]) {
+                count++;
+            } else {
+                count = 1;
+            }
+            if (max < count) {
+                max = count;
+            }
+        }
+        return max;
+    }
+}
