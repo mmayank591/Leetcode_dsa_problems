1d880fe5e1fe5c858b445bcc327fa620f80dea1d
Mayank Mishra <89137604+mmayank591@users.noreply.github.com>
2023-11-06 20:54:03 +0530

    Create Seat_Reservation_Manager.java

---
+++ Seat_Reservation_Manager.java
@@ -0,0 +1,21 @@
+class SeatManager {
+    private TreeSet<Integer> seats;
+
+    public SeatManager(int n) {
+        seats = new TreeSet();
+        for(int i=1;i<=n;i++)
+        {
+            seats.add(i);
+        }
+
+        
+    }
+    
+    public int reserve() {
+        return seats.pollFirst();
+    }
+    
+    public void unreserve(int seatNumber) {
+        seats.add(seatNumber);
+    }
+}
