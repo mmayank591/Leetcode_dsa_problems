ee550511d06dee2575328d05de2e2296892af40b
Mayank Mishra <89137604+mmayank591@users.noreply.github.com>
2023-11-08 10:56:38 +0530

    Create 2849. Determine if a Cell Is Reachable at a Given Time.java

---
+++ 2849. Determine if a Cell Is Reachable at a Given Time.java
@@ -0,0 +1,14 @@
+https://leetcode.com/problems/determine-if-a-cell-is-reachable-at-a-given-time/description/
+
+class Solution {
+    public boolean isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+        int xdiff=Math.abs(sx-fx);
+        int ydiff=Math.abs(sy-fy);
+if(xdiff==0 && ydiff==0 && t==1){
+    return false;
+
+}
+return (Math.min(xdiff,ydiff)+ Math.abs(xdiff-ydiff) <=t);
+
+    }
+}
