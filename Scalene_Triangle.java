7f785ad28a80beaa987fb1bf1d879cdf2aa7df8b
Mayank Mishra <89137604+mmayank591@users.noreply.github.com>
2024-02-03 23:28:39 +0530

    Create Scalene_Triangle.java

---
+++ Scalene_Triangle.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+
+        while (t-- > 0) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            int c = scanner.nextInt();
+            // Your code goes here
+            if((a!=b)&&(b!=c)&&(c!=a))
+            {
+                System.out.println("YES");
+            }
+            else
+            {
+                System.out.println("NO");
+            }
+        }
+    }
+}
