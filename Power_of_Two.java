47a57b2480c65d06136c907016518664d8903206
Mayank Mishra <89137604+mmayank591@users.noreply.github.com>
2024-02-20 01:13:52 +0530

    Create Power_of_Two.java

---
+++ Power_of_Two.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0)
+        {
+            return false;
+        }
+        if(n==1)
+        {
+            return true;
+        }
+        return (isPowerOfTwo(n/2) && n%2==0);
+    }
+}
