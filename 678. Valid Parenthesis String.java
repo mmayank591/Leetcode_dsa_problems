70460e138f45bd7aa58e387211d92d04fd522d58
Mayank Mishra <89137604+mmayank591@users.noreply.github.com>
2024-04-07 11:05:16 +0530

    Create 678. Valid Parenthesis String.java

---
+++ 678. Valid Parenthesis String.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean checkValidString(String s) {
+
+        Stack<Integer> left, star;
+        left = new Stack<>();
+        star = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                left.push(i);
+            } else if (s.charAt(i) == '*') {
+                star.push(i);
+            } else {
+                if (!left.isEmpty()) {
+                    left.pop();
+                } else if (!star.isEmpty()) {
+                    star.pop();
+                } else {
+                    return false;
+                }
+
+            }
+
+        }
+        while (left.size() != 0) {
+            if (star.size() == 0) {
+                return false;
+            }
+            if (star.peek() > left.peek()) {
+                star.pop();
+                left.pop();
+            } else {
+                return false;
+            }
+
+        }
+        return true;
+
+    }
+}
