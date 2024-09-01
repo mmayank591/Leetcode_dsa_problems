0cb11184de4376e5ee8b3edd270c61a4eba6cfa8
Mayank Mishra <89137604+mmayank591@users.noreply.github.com>
2024-09-01 12:18:31 +0530

    Create 2053. Kth Distinct String in an Array.java

---
+++ 2053. Kth Distinct String in an Array.java
@@ -0,0 +1,23 @@
+https://leetcode.com/problems/kth-distinct-string-in-an-array/description/?envType=daily-question&envId=2024-09-01
+
+class Solution {
+    public String kthDistinct(String[] arr, int k) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (map.get(arr[i]) == 1) {
+                count++;
+            }
+            if (count == k) {
+                return arr[i];
+            }
+        }
+        return "";
+    }
+}
